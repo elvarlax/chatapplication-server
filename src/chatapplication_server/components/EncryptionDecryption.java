f644ba3e9bf96e9747c99715dfc90ea8baf0a6d2
Roberts <robis1221@inbox.lv>
2020-09-17 16:22:05 +0200

    created the class for encryption/decryption

---
+++ src/chatapplication_server/components/EncryptionDecryption.java
@@ -0,0 +1,14 @@
+package chatapplication_server.components;
+
+public class EncryptionDecryption {
+    private static String sharedKey = "";
+    public static void setSharedKey(String key){
+
+    }
+    public static String Encrypt(String message){
+        return "";
+    }
+    public static String Decrypt(String message){
+        return "";
+    }
+}
