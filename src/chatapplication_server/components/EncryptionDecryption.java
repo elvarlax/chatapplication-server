90c36ade4e5043263c56bea6224e9de754ca5886
Roberts <robis1221@inbox.lv>
2020-09-17 17:01:32 +0200

    added setSharedKey method

--- src/chatapplication_server/components/EncryptionDecryption.java
+++ src/chatapplication_server/components/EncryptionDecryption.java
@@ -1,13 +1,22 @@
 package chatapplication_server.components;
 
 import java.math.BigInteger;
-
-import org.bouncycastle.util.BigIntegers;
+import java.security.MessageDigest;
 
 
 public class EncryptionDecryption {
     private static String sharedKey = "";
     public static void setSharedKey(BigInteger key){
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(key.byteValue());
+            byte[] digest = md.digest();
+            String shaKey = String.format("%064x", new BigInteger(1, digest));
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+
 
 
     }
