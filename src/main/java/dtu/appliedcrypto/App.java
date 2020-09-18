da3150156d20f7a3eae0e5190ca7bc27a9540424
Tomas Izo <tomas@izo.sk>
2020-09-18 18:32:28 +0200

    converting to maven project

---
+++ src/main/java/dtu/appliedcrypto/App.java
@@ -0,0 +1,42 @@
+package dtu.appliedcrypto;
+
+import java.io.IOException;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import dtu.appliedcrypto.chatapplication_server.ChatApplicationServerEngine;
+
+public class App {
+    public static void SetupSecurity() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static void main(String[] args) {
+        String mode = ChatApplicationServerEngine.getCommandLineArgPasswd(args);
+
+        System.out.println(args[0]);
+        System.out.println(mode);
+
+        SetupSecurity();
+
+        if (mode.equals("Server")) {
+            /** Boot up the ChatApplicationServer system... */
+            ChatApplicationServerEngine.getInstance().startUpCAServer();
+        } else if (mode.equals("Client")) {
+            /** Boot up the ChatApplicationClient system... */
+            ChatApplicationServerEngine.getInstance().startUpCAClient();
+        } else {
+            System.out.println("No correct mode given - either 'Mode=Server' or 'Mode=Client'");
+            System.exit(1);
+        }
+
+        /** Wait for a keypress; prevent other threads from dying */
+        try {
+            System.in.read();
+        } catch (IOException ioe) {
+        }
+
+        /** Safely shut down the LSCS system... */
+        ChatApplicationServerEngine.getInstance().shutDownCA();
+    }
+}
