ac3a40e60dc09e3673c340edb961f80aaa70c962
Tomas Izo <tomas@izo.sk>
2020-10-17 14:15:12 +0200

    some tests

--- src/test/java/dtu/appliedcrypto/AppTest.java
+++ src/test/java/dtu/appliedcrypto/AppTest.java
@@ -1,20 +1,31 @@
 package dtu.appliedcrypto;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import java.security.cert.Certificate;
+
+import dtu.appliedcrypto.chatapplication_server.certs.Certificates;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldLoadKeyStore() throws Exception {
+        String keyStore = "certificates/Alice.jks";
+        String keyPass = "123456";
+        Certificates certs = new Certificates(keyStore, keyPass);
+        assertNotNull(certs);
+    }
+
+    @Test
+    public void shouldLoadAlicesCerts() throws Exception {
+        String keyStore = "certificates/Alice.jks";
+        String keyPass = "123456";
+        Certificates certs = new Certificates(keyStore, keyPass);
+        Certificate cert = certs.getCert("Alice");
+        assertNotNull(cert);
     }
 }
