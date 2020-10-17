c2576bc27c93e893c45def2ce9e6f8765efff56f
Roberts <robis1221@inbox.lv>
2020-10-17 11:40:29 +0200

    Added certificates class with importing key store and extracting public key

---
+++ src/main/java/dtu/appliedcrypto/chatapplication_server/certs/Certificates.java
@@ -0,0 +1,31 @@
+package dtu.appliedcrypto.chatapplication_server.certs;
+
+import java.security.KeyStore;
+import java.security.PublicKey;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.security.cert.Certificate;
+
+public class Certificates {
+    KeyStore ks;
+    FileInputStream ksfis;
+    BufferedInputStream ksbufin;
+    final String ownAlias="Bob";
+    final String keyStore = "KeyStore.jks";
+    final String keyStorePass = "123456";
+    public Certificates() throws Exception{
+        ks = KeyStore.getInstance( "JKS" );
+        ksfis = new java.io.FileInputStream( keyStore );
+        ksbufin = new java.io.BufferedInputStream( ksfis );
+        ks.load( ksbufin, keyStorePass.toCharArray() );
+    }
+    public boolean verifyCert(){
+        //TO-DO
+        return false;
+    }
+    public PublicKey extractPublicKey(String alias) throws Exception{
+        Certificate cert = ks.getCertificate( alias );
+        PublicKey pubKey = cert.getPublicKey();
+        return pubKey;
+    }
+}
