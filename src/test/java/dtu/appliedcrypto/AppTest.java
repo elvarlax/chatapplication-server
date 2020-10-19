240a06c3c306d7f9dc9abd0b89e63f8642cad580
Elvar Laxdal <elvarlax@gmail.com>
2020-10-19 13:37:23 +0200

    Fixing encryptAndDecryptKey test to pass

--- src/test/java/dtu/appliedcrypto/AppTest.java
+++ src/test/java/dtu/appliedcrypto/AppTest.java
@@ -1,18 +1,15 @@
 package dtu.appliedcrypto;
 
 import dtu.appliedcrypto.chatapplication_server.certs.Certificates;
-import dtu.appliedcrypto.chatapplication_server.crypto.DiffieHellman;
 import dtu.appliedcrypto.chatapplication_server.crypto.PublicKeyCrypto;
-
-import java.math.BigInteger;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.FileInputStream;
 import java.security.SignatureException;
 import java.security.cert.Certificate;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -48,13 +45,13 @@ public class AppTest {
     public void encryptAndDecryptKey() throws Exception {
         PublicKeyCrypto pkc = new PublicKeyCrypto();
         byte[] key = pkc.generateSharedKey();
-        
+
         String keyStore = "certificates/AliceKeyStore.jks";
         String keyPass = "123456";
         Certificates certificates = new Certificates(keyStore, keyPass);
         Certificate alice = certificates.getCert("Alice");
         byte[] encrKey = pkc.encryptText(key, alice.getPublicKey());
         byte[] decrKey = pkc.decryptText(encrKey, certificates.getPrivateKey("alice", "123456"));
-        assertEquals(decrKey, key);
+        assertEquals(Arrays.toString(decrKey), Arrays.toString(key));
     }
 }
